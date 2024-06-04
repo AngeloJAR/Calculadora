35060a82caa6cbdcec16a4d6d3ec7ec9a9b87a0f
Angelo <Angelo@DESKTOP-GKCIKU1>
2024-06-04 08:39:32 -0500

    Agregar archivos de proyecto.

---
+++ Tarea1/Tarea1/Controlador/db_Conexion.cs
@@ -0,0 +1,38 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Tarea1.Models;
+
+
+namespace Tarea1.Controlador
+{
+    public class db_Conexion
+    {
+        public SQLiteAsyncConnection _database;
+
+        public db_Conexion(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Historial>().Wait();
+        }
+
+        // Métodos para la tabla Historial
+        public Task<List<Historial>> GetOperacionesAsync()
+        {
+            return _database.Table<Historial>().ToListAsync();
+        }
+
+        public Task<int> SaveOperacionAsync(Historial operacion)
+        {
+            return _database.InsertAsync(operacion);
+        }
+
+        public Task<int> DeleteOperacionAsync(Historial operacion)
+        {
+            return _database.DeleteAsync(operacion);
+        }
+    }
+}
---
+++ Tarea1/Tarea1/Models/Historial.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+namespace Tarea1.Models
+{
+    public class Historial
+    {
+        [PrimaryKey, AutoIncrement]
+        public int id { get; set; }
+        public string Fecha { get; set; }
+        public string operacion { get; set; }
+        public string Numero1 { get; set; }
+        public string Numero2 { get; set; }
+        public string Resultado { get; set; }
+    }
+}

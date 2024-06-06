99af882bea36dd480add0ea211ffd1b36ee25cff
Angelo <Angelo@DESKTOP-GKCIKU1>
2024-06-05 21:03:45 -0500

    Boton con imagen

--- Tarea1/Tarea1/MainPage.xaml.cs
+++ Tarea1/Tarea1/MainPage.xaml.cs
@@ -1,127 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tarea1.Controlador;
-using Tarea1.Models;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace Tarea1
 {
-    public partial class MainPage : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : FlyoutPage
     {
-        //Se almacena la operacion actual a realizar, luego se almacena el historial de opreaciones, y finalmente se realiza la conexion a la base de datos
-        private string currentOperation;
-        private ObservableCollection<Historial> operaciones;
-        private db_Conexion database;
         public MainPage()
         {
             InitializeComponent();
-            //Se defne la ruta de la base de datos, luego se iniciza la conexion
-            var dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Operaciones.db3");
-            database = new db_Conexion(dbPath);
-            operaciones = new ObservableCollection<Historial>();
-            //Se establece la tabla para el historial como un listview
-            HostorialListView.ItemsSource = operaciones;
-            //carga de operaciones existentes desde la base de datos
-            LoadOperaciones();
+           
         }
-        //metodo para cargar las operaciones
-        private async Task LoadOperaciones()
-        {
-            //obtiene las operaciones de la DB y agrega la operacion a una coleccion
-            var ops = await database.GetOperacionesAsync();
-            foreach (var op in ops)
-            {
-                operaciones.Add(op);
-            }
-        }
-        //metodo para el boton de las operacions
-        private void operacionesSRMD(object sender, EventArgs e)
-        {
-            //almacena, muestra campos para el ingreso de numeros, limpia los labels para las operaciones
-            var button = (Button)sender;
-            currentOperation = button.Text;
-            IngresoNumeros.IsVisible = true;
-            ResultadoLabel.Text = "";
-            Numero1Entry.Text = "";
-            Numero2Entry.Text = "";
-        }
-        //metodos para el boton de calcular
-        private async void btnCalcular(object sender, EventArgs e)
-        {
-            //primero verifica que ambos campos no esten vacios si lo estan nos da un mensaje para rellenar los campos
-            if (string.IsNullOrEmpty(Numero1Entry.Text) || string.IsNullOrEmpty(Numero2Entry.Text))
-            {
-                await DisplayAlert("Error", "Por favor, ingrese ambos números.", "OK");
-                return;
-            }
-            //convertir los numero de entrada a numeros
-            if (!double.TryParse(Numero1Entry.Text, out double numero1) || !double.TryParse(Numero2Entry.Text, out double numero2))
-            {
-                await DisplayAlert("Error", "Por favor, ingrese números válidos.", "OK");
-                return;
-            }
 
-            double resultado = 0; //variable que almacena el resultado de la operacions
-            //menu para realizar la operacion seleccionada
-            switch (currentOperation)
-            {
-                case "Suma":
-                    resultado = numero1 + numero2;
-                    break;
-                case "Resta":
-                    resultado = numero1 - numero2;
-                    break;
-                case "Multiplicación":
-                    resultado = numero1 * numero2;
-                    break;
-                case "División":
-                    if (numero2 != 0)
-                    {
-                        resultado = numero1 / numero2;
-                    }
-                    else
-                    {
-                        await DisplayAlert("Error", "No se puede dividir entre cero.", "OK");
-                        return;
-                    }
-                    break;
-            }
-            //mostrar el resultado 
-            ResultadoLabel.Text = "Resultado: " + resultado.ToString();
-
-            //crear una nueva instancia con los datos de la operacion es decir solo con esos datos ingresados se realiza la operacion 
-            var operacion = new Historial
-            {
-                Fecha = DateTime.Now.ToString("g"), //nos da la fecha y la hora acutal
-                operacion = currentOperation,
-                Numero1 = numero1.ToString(),
-                Numero2 = numero2.ToString(),
-                Resultado = resultado.ToString()
-            };
-            //guarda la operacion y añade la operacion a la tabla 
-            await database.SaveOperacionAsync(operacion);
-            operaciones.Add(operacion);
-            //limpiar campos
-            Numero1Entry.Text = "";
-            Numero2Entry.Text = "";
-        }
-        //metodo para el boton eliminar
-        private async void btnEliminar(object sender, EventArgs e)
-        {
-            
-            var button = (Button)sender;
-            //obtener la operacion vinculada al boton 
-            var operacion = (Historial)button.CommandParameter;
-            //elimina la operacion de la DB
-            await database.DeleteOperacionAsync(operacion);
-            //eliminar la operacion de la tabla
-            operaciones.Remove(operacion);
-        }
+    
     }
 }
---
+++ Tarea1/Tarea1/MainPageFlyout.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Tarea1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPageFlyout : ContentPage
+    {
+        public ListView ListView;
+
+        public MainPageFlyout()
+        {
+            InitializeComponent();
+        }
+
+        //private async void OnInicioClicked(object sender, EventArgs e)
+        //{
+        //    var parent = (FlyoutPage)Parent;
+        //    parent.Detail = new NavigationPage(new vistas.Calculadora());
+        //    parent.IsPresented = false;
+        //}
+
+        //private async void OnAcercaDeClicked(object sender, EventArgs e)
+        //{
+        //    var parent = (FlyoutPage)Parent;
+        //    parent.Detail = new NavigationPage(new vistas.AcercaDePage());
+        //    parent.IsPresented = false;
+        //}
+
+        //private async void OnIniciarSesionClicked(object sender, EventArgs e)
+        //{
+        //    var parent = (FlyoutPage)Parent;
+        //    parent.Detail = new NavigationPage(new vistas.IniciarSesionPage());
+        //    parent.IsPresented = false;
+        //}
+        private async void btnImagenes(object sender, EventArgs e)
+        {
+            var parent = (FlyoutPage)Parent;
+            parent.Detail = new NavigationPage(new vistas.Imagenes());
+            parent.IsPresented = false;
+        }
+        private async void btnCalculadora(object sender, EventArgs e)
+        {
+            var parent = (FlyoutPage)Parent;
+            parent.Detail = new NavigationPage(new vistas.Calculadora());
+            parent.IsPresented = false;
+        }
+    }
+}
